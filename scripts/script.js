f38859a12d7eb2ec2c3024fa9459e4ae80e3ccf1
Sean <38589300+CoolCoolSweet@users.noreply.github.com>
2018-06-06 01:03:48 -0400

    responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $( function() {
 
 	// Variables
-	const splashText = "Sean McNeil. Web Developer"
+	let splashText = (window.innerWidth < 600 && window.innerHeight > 449) ? "Sean~ McNeil. Web Developer" : "Sean McNeil. Web Developer";
 	const splashTypeTiming = 110;
 	let repeatSplashTyping = true;
 	const repeatSplashTypingPause = 5000;
@@ -11,7 +11,10 @@ $( function() {
 		let setIntervalIndex = 0;
 		$('#home .wrapper').append(`<h1 class="splash-text"></h1>`);
 		setInterval(() => {
-			if (splashText[setIntervalIndex] === "." && setIntervalIndex + 1 < splashText.length) {
+			if (splashText[setIntervalIndex] === "~") {
+				$('#home .wrapper').append(`<h1 class="splash-text">${splashText[setIntervalIndex + 1]}</h1>`);
+				setIntervalIndex += 2;
+			} else if (splashText[setIntervalIndex] === "." && setIntervalIndex + 1 < splashText.length) {
 				$('#home .wrapper').append(`<h2 class="splash-text">${splashText[setIntervalIndex + 1]}</h2>`);
 				setIntervalIndex += 2;
 			} else if (setIntervalIndex < splashText.length) {
@@ -24,6 +27,7 @@ $( function() {
 					setIntervalIndex = 0;
 					repeatSplashTyping = true;
 					$('#home .wrapper').append(`<h1 class="splash-text"></h1>`);
+					splashText = (window.innerWidth < 600 && window.innerHeight > 449) ? "Sean~ McNeil. Web Developer" : "Sean McNeil. Web Developer";
 				}, repeatSplashTypingPause);
 			}
 		}, splashTypeTiming);
